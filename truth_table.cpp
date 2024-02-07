7a4f63794600e8f2af220f869b99fefe10d7670a
Isaac Denny <isaac_denny@icloud.com>
2024-02-07 10:12:27 -0500

    recursive stuff

--- truth_table.cpp
+++ truth_table.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 using namespace std;
 
+// This works, but uses an if statement and 2 loops
+// void printTable(unsigned char n) {
+//     unsigned char i, j;
+//     for (i = 1 << 0; i <= (1 << n); i++) {
+//         for (j = 1 << n; j > 0; j = j / 2)
+//         {
+//             cout << (int)((((i - 1) & j) >> (int)(i / n)) & 1);
+//             // if (((i - 1) & j) != 0)
+//             // {
+//             //     cout << "1 ";
+//             // }
+//             // else
+//             // {
+//             //     cout << "0 ";
+//             // }
+//         }
+//         cout << endl;
+//     }
+// }
+
+// This also works, but it uses an if statement and recursive
+int decimalToBinary(int n) {
+    if (n == 0) return 0;
+    else return (n % 2 + 10 * decimalToBinary(n / 2));
+}
+
+
 void printTable(unsigned char n) {
     unsigned char i, j;
-    for (i = 1 << 0; i <= (1 << n); i++) {
-        for (j = 1 << n; j > 0; j = j / 2)
-        {
-            // cout << (int)((i - 1) & j);
-            if (((i - 1) & j) != 0)
-            {
-                cout << "1 ";
-            }
-            else
-            {
-                cout << "0 ";
-            }
-        }
+    for (i = 0; i < n; i++) {
+        cout << decimalToBinary(i);
+        // for (j = i; j > 0; j = j >> 1) {
+        //     cout << (j & 1);
+        // }
         cout << endl;
     }
 }
@@ -23,5 +42,6 @@ void printTable(unsigned char n) {
 // Driver Code
 int main()
 {
-    printTable(2);
+    printTable(4);
+    return 0;
 }
